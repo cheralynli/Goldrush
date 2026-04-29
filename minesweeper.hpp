96eb95b6670f55302064075d727fcbc3bf3bbdc7
Mich <liu.michelle2007@gmail.com>
2026-04-29 22:58:33 +0800

    minesweeper.hpp

--- minesweeper.hpp
+++ minesweeper.hpp
@@ -2,6 +2,10 @@
 
 #include <string>
 
+//Input: none 
+//Output: holds the outcome of a Minesweeper minigame run
+//Purpose: encapsulates the player’s performance and game state
+//Relation: returned by playMinesweeperMinigame() to report results
 struct MinesweeperResult {
     int safeTilesRevealed;
     bool hitBomb;
